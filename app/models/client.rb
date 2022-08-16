c9fd9cabe9e4b36546efe6a2cb45fdf76701cd31
Mauro Almiron <mdavid.almiron@gmail.com>
2022-08-16 15:03:59 -0300

    Change client code no es unico

--- app/models/client.rb
+++ app/models/client.rb
@@ -22,7 +22,7 @@ class Client < ApplicationRecord
 	validates :name, presence: true
 	validates :last_name, presence: true
 	validates :dni, uniqueness: { message: "Este dni pertenece a otro cliente" }, allow_blank: true
-	validates :code, uniqueness: { message: "Este legajo pertenece a otro cliente" }, allow_blank: true
+	# validates :code, uniqueness: { message: "Este legajo pertenece a otro cliente" }, allow_blank: true
 
 	def fullname
 		"#{self.last_name} #{self.name}"	
