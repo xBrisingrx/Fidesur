01c37acabfa3d3b2101220a47c4afbc776be836a
Mauro Almiron <mdavid.almiron@gmail.com>
2022-09-06 09:11:16 -0300

    add relacion entre usuario y land create/actualizada

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,12 @@
 #
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :lands_created, class_name: 'Land', 
+                            foreign_key: 'user_created_id'
+  has_many :lands_updated, class_name: 'Land',
+                            foreign_key: 'user_updated_id'
+
   validates :username, presence: true, uniqueness: { case_sensitive: false, message: "Este usuario ya se encuentra registrado" }
   validates :email, presence: true, uniqueness: { case_sensitive: false, message: "Este email de usuario se encuentra en uso" }
 end
