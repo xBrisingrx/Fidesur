820532e508f685a4ad64fa244b32801af086a91b
Mauro Almiron <mdavid.almiron@gmail.com>
2022-09-06 09:09:11 -0300

    tuve que usar el campo

--- db/migrate/20220903211318_create_lands.rb
+++ db/migrate/20220903211318_create_lands.rb
@@ -8,6 +8,7 @@ class CreateLands < ActiveRecord::Migration[5.2]
       t.decimal :price, precision: 15, scale: 2, default: 0
       t.integer :status, default: 0
       t.integer :land_type, default: 0
+      t.boolean :is_green_space, default: 0
       t.decimal :space_not_available,precision: 15, scale: 2, default: 0.0, comment: 'Espacio de el lote que no puede ser utilizado'
       t.references :apple, foreign_key: true
       t.references :user_created, foreign_key: { to_table: 'users' }
