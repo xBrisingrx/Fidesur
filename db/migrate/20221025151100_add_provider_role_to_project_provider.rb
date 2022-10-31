a561e6b406c0402331ba94cf1fa2a9c2c6c935ab
David <mdavid.almiron@gmail.com>
2022-10-31 15:20:02 -0300

    fix foreign name

--- db/migrate/20221025151100_add_provider_role_to_project_provider.rb
+++ db/migrate/20221025151100_add_provider_role_to_project_provider.rb
@@ -1,5 +1,5 @@
 class AddProviderRoleToProjectProvider < ActiveRecord::Migration[5.2]
   def change
-    add_reference :project_providers, :provider_roles, foreign_key: true
+    add_reference :project_providers, :provider_role, foreign_key: true
   end
 end
--- db/migrate/20221026032032_add_payment_method_to_project_provider.rb
+++ db/migrate/20221026032032_add_payment_method_to_project_provider.rb
@@ -1,5 +1,5 @@
 class AddPaymentMethodToProjectProvider < ActiveRecord::Migration[5.2]
   def change
-    add_reference :project_providers, :payment_methods, foreign_key: true
+    add_reference :project_providers, :payment_method, foreign_key: true
   end
 end
