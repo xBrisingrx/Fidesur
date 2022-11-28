7801d235e2b071066405f427f7c192a8c3c0d2fa
Mauro Almiron <mdavid.almiron@gmail.com>
2022-11-28 16:19:07 -0300

    batchpayment nunca se uso

--- app/assets/javascripts/batch_payments.js
+++
@@ -1,6 +0,0 @@
-var batch_payment_data = {
-	valor_cuota: null,
-	recargo_sugerido: null,
-	ajuste: null,
-	total_a_pagar: null
-}
--- test/controllers/batch_payments_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class BatchPaymentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
